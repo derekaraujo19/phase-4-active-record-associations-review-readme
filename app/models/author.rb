a280ac52db216e7d4cd16e3b5062d5caeafd9282
Derek Araujo <derekaraujo19@gmail.com>
2022-09-22 18:25:16 -0400

    commit tree 2e4467b25bfdb16a8b980b63f561587915187f6d

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ApplicationRecord
+  has_many :posts
+
+  def post_titles
+    self.posts.map do |post|
+      post.title
+    end
+  end
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  belongs_to :author
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  def self.post_titles
+    Post.all.map do |post|
+      post.title
+    end
+  end
 end
